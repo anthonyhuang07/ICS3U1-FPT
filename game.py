32315ceb870cd83343b9717d45fe751654781d1a
Anthony Huang <anthonyhuang07@icloud.com>
2024-01-15 12:19:02 -0500

    CHECKPOINT 2

--- game.py
+++ game.py
@@ -25,15 +25,15 @@ c2 = 3 * 1000
 c3 = 6 * 1000
 c4 = 12 * 1000
 c5 = 24 * 1000
-c6 = 96 * 1000
-c7 = 384 * 1000
-c8 = 1536 * 1000
+c6 = 48 * 1000
+c7 = 96 * 1000
+c8 = 192 * 1000
 
 canClick = [s1, s2, s3, s4, s5, s6, s7, s8]
 timers = [tm1, tm2, tm3, tm4, tm5, tm6, tm7, tm8]
 cooldowns = [c1, c2, c3, c4, c5, c6, c7, c8]
 
-# task coordinates
+# task box coordinates
 boxW = 385
 boxH = 100
 boxRX = 100
@@ -43,7 +43,7 @@ boxY2 = 295
 boxY3 = 430
 boxY4 = 565
 
-# buy button coordinates
+# buy (worker) button coordinates
 buyW = boxW / (4 / 3) / 1.4 + 6
 buyH = boxH / 2
 buyRX = boxRX + boxW / 4 - 5
@@ -52,7 +52,6 @@ buyY1 = boxY1 + boxH / 2
 buyY2 = boxY2 + boxH / 2
 buyY3 = boxY3 + boxH / 2
 buyY4 = boxY4 + boxH / 2
-
 
 # colors
 LIGHT1 = (22, 219, 101)
@@ -129,6 +128,9 @@ coefficients = [CF1, CF2, CF3, CF4, CF5, CF6, CF7, CF8]
 a1 = a2 = a3 = a4 = a5 = a6 = a7 = a8 = 1
 amounts = [a1, a2, a3, a4, a5, a6, a7, a8]
 
+multStatus = 0
+buyMultiplier = 1
+
 
 # function that centers text input - all parameters specified
 def centerText(txt, font, col, x, y, w, h):
@@ -140,6 +142,17 @@ def centerText(txt, font, col, x, y, w, h):
     at = font.render(a, 1, col)
     screen.blit(at, ar)
 
+def numberText(num):
+    number = "%.2f" % num
+    if num >= 1000000000000:
+        number = "%.2fT" % (num/1000000000000)
+    elif num >= 1000000000:
+        number = "%.2fB" % (num/1000000000)
+    elif num >= 1000000:
+        number = "%.2fM" % (num/1000000)
+    elif num >= 1000:
+        number = "%.2fK" % (num/1000)
+    return number
 
 # draws the task boxes
 def task(statVar, x, y, n):
@@ -147,41 +160,19 @@ def task(statVar, x, y, n):
 
     button = pygame.Rect(x, y, boxW, boxH)
 
-    if money >= calcReqCost(n) and statVar == 0:  # Can Buy - Buy Task turns Green
+    if money >= calcReqCost(n) * buyMultiplier and statVar == 0:  # Can Buy - Buy Task turns Green
         pygame.draw.rect(screen, LIGHT1, button)
         centerText(names[n - 1], light, DARK4, x, y - 20, boxW, boxH)
-        centerText("$%.2f" % calcReqCost(n), light, DARK4, x, y + 20, boxW, boxH)
+        centerText(numberText(calcReqCost(n) * buyMultiplier), light, DARK4, x, y + 20, boxW, boxH)
     elif statVar == 0:  # Cannot Buy - Buy Task stays Black
         pygame.draw.rect(screen, LIGHT1, button, 2)
         centerText(names[n - 1], light, LIGHT1, x, y - 20, boxW, boxH)
-        centerText("$%.2f" % calcReqCost(n), light, LIGHT1, x, y + 20, boxW, boxH)
+        centerText(numberText(calcReqCost(n) * buyMultiplier), light, LIGHT1, x, y + 20, boxW, boxH)
     elif statVar == 1:  # Task is Bought...
-        inc = BIarr[n - 1] * amounts[n - 1] # income
-        centerText(
-            "$%.2f" % inc,
-            regularS,
-            LIGHT1,
-            x + boxW / 4 - 5,
-            y + 4,
-            boxW / (4 / 3) + 7,
-            boxH / 2,
-        ) # 
-
-        # boxes
-        pygame.draw.rect(screen, LIGHT1, (x, y, boxW, boxH), 5)  # whole task box
-        pygame.draw.rect(screen, LIGHT1, (x, y, boxW / 4, boxH))  # left hand box
-        pygame.draw.rect(
-            screen,
-            LIGHT1,
-            (x + boxW / 4 - 5, y + boxH / 2, boxW / (4 / 3) + 6, boxH / 2),
-            5,
-        )  # bottom action box - Buy and Cooldown
-
-        centerText(str(amounts[n - 1]), regularS, DARK4, x, y + boxH / 2, boxW / 4, boxH / 2)
-
         # COOLDOWN, AND ADD MONEY WHEN COOLDOWN ENDS
         if canClick[n-1] == False:
             cd = cooldowns[n-1] - (pygame.time.get_ticks() - timers[n-1])
+            pygame.draw.rect(screen, DARK1, (x + boxW / 4, y+5, (boxW / (4 / 3) - 4) - (cd/(cooldowns[n-1]/(boxW / (4 / 3) - 4))), boxH / 2))
             if cd <= 0:
                 canClick[n-1] = True
                 money += BIarr[n - 1] * amounts[n - 1]
@@ -196,15 +187,30 @@ def task(statVar, x, y, n):
             cooldown = '{mins:02d}:{secs:02d}'.format(mins=mins, secs=secs)
             centerText(cooldown, lightS, LIGHT1, x + boxW / 4 - 10 + boxW / (4 / 3) / 1.4 + 6, y + boxH / 2 + 1, boxW / (4.66) + 5, boxH / 2)
 
+        # boxes
+        pygame.draw.rect(screen, LIGHT1, (x, y, boxW, boxH), 5)  # whole task box
+        pygame.draw.rect(screen, LIGHT1, (x, y, boxW / 4, boxH))  # left hand box
+        pygame.draw.rect(
+            screen,
+            LIGHT1,
+            (x + boxW / 4 - 5, y + boxH / 2, boxW / (4 / 3) + 6, boxH / 2),
+            5,
+        )  # bottom action box - Buy and Cooldown
+
+        centerText(str(amounts[n - 1]), regularS, DARK4, x, y + boxH / 2, boxW / 4, boxH / 2)
+
+        inc = BIarr[n - 1] * amounts[n - 1] # income
+        centerText(numberText(inc),regularS,LIGHT1,x + boxW / 4 - 5,y + 4,boxW / (4 / 3) + 7,boxH / 2)
+
         # WORKER BUYING LOGIC
-        if money >= calcTaskCost(n):  # Can buy Worker - Buy button turns Green
+        if money >= calcTaskCost(n) * buyMultiplier:  # Can buy Worker - Buy button turns Green
             pygame.draw.rect(
                 screen,
                 LIGHT1,
                 (x + boxW / 4 - 5, y + boxH / 2, boxW / (4 / 3) / 1.4 + 6, boxH / 2),
             )
             centerText(
-                "Buy x1 - %.2f" % calcTaskCost(n),
+                "Buy x%i - %.2f" % (buyMultiplier, calcTaskCost(n) * buyMultiplier),
                 lightS,
                 DARK4,
                 x + boxW / 4 - 5,
@@ -220,7 +226,7 @@ def task(statVar, x, y, n):
                 5,
             )
             centerText(
-                "Buy x1 - %.2f" % calcTaskCost(n),
+                "Buy x%i - %.2f" % (buyMultiplier, calcTaskCost(n) * buyMultiplier),
                 lightS,
                 LIGHT1,
                 x + boxW / 4 - 5,
@@ -234,12 +240,13 @@ def taskClick(n, x, y, bx, by):
     global money
 
     if mx > x and mx < x + boxW and my > y and my < y + boxH:
-        if statuses[n - 1] == 0 and money >= calcReqCost(n):  # buy task
+        if statuses[n - 1] == 0 and money >= calcReqCost(n) * buyMultiplier:  # buy task
             statuses[n - 1] = 1
-            money -= calcReqCost(n)
+            amounts[n-1] = buyMultiplier
+            money -= calcReqCost(n) * buyMultiplier
         elif (
             statuses[n - 1] == 1
-            and money < calcTaskCost(n)
+            and money < calcTaskCost(n) * buyMultiplier
             and mx > bx
             and mx < bx + buyW
             and my > by
@@ -248,14 +255,14 @@ def taskClick(n, x, y, bx, by):
             return
         elif (
             statuses[n - 1] == 1
-            and money >= calcTaskCost(n)
+            and money >= calcTaskCost(n) * buyMultiplier
             and mx > bx
             and mx < bx + buyW
             and my > by
             and my < by + buyH
         ):  # buy worker
-            money -= calcTaskCost(n)
-            amounts[n - 1] += 1
+            money -= calcTaskCost(n) * buyMultiplier
+            amounts[n - 1] += buyMultiplier
         elif statuses[n - 1] == 1:
             if canClick[n-1] == True:
                 timers[n-1] = pygame.time.get_ticks()
@@ -284,6 +291,15 @@ while playing:
             else:
                 taskClick(i, boxRX2, yPos[count - 1], buyRX2, yPos[-count])
                 count += 1
+        if mx >= 860 and mx <= 980 and my >= 20 and my <= 70:
+            multStatus += 1
+            if multStatus == 1:
+                buyMultiplier = 10
+            elif multStatus == 2:
+                buyMultiplier = 100
+            elif multStatus == 3:
+                multStatus = 0
+                buyMultiplier = 1
 
         button = 0
 
@@ -293,7 +309,11 @@ while playing:
 
     # header
     pygame.draw.rect(screen, LIGHT1, (1, 1, 999, 125), 2)
-    centerText("$%.2f" % money,medium,LIGHT1,1-325,1,999,124)
+    text = medium.render("$%.2f" % money, 1, LIGHT1)
+    screen.blit(text, (100,27.5))
+
+    pygame.draw.rect(screen, LIGHT1, (860, 20, 120, 50))
+    centerText("Buy x%i" % buyMultiplier, regularS, DARK4, 860, 20, 120, 50)
 
     ## TASKS ##
 
